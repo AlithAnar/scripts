161b030b18aa5bb147ee02db59a5f34789d01f7c
AlithAnar <alith.anar.ensiferum@gmail.com>
2016-06-09 21:33:58 +0200

    up

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -13,12 +13,14 @@ sudo add-apt-repository ppa:nilarimogard/webupd8 -y
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install pulseaudio-equalizer fontconfig-infinality bum baobab steam openvpn gksu guake vim google-chrome-stable bleachbit sublime-text-installer git vlc ntfs-config grub-customizer oracle-java8-installer deluge p7zip-full curl software-properties-common kodi gufw clementine y-ppa-manager -y
+sudo apt-get install python-pip pulseaudio-equalizer fontconfig-infinality bum baobab steam openvpn gksu guake vim google-chrome-stable bleachbit sublime-text-installer git vlc ntfs-config grub-customizer oracle-java8-installer deluge p7zip-full curl software-properties-common kodi gufw clementine y-ppa-manager -y
 curl -sL https://raw.githubusercontent.com/brainsik/virtualenv-burrito/master/virtualenv-burrito.sh | $SHELL
 gconftool-2 -t bool -s /apps/gnome-terminal/profiles/Default/login_shell true
 sudo apt-get install --install-recommends wine-staging winehq-staging -y
 sudo bash /etc/fonts/infinality/infctl.sh setstyle
-
+sudo pip install glances
+wget -O adobe-air.sh http://drive.noobslab.com/data/apps/AdobeAir/adobe-air.sh
+chmod +x adobe-air.sh;sudo ./adobe-air.sh
 
 
 sudo apt-get autoclean
